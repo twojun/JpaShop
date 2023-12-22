6aac6baa425ce694710a292f5d26c721f683bffa
yoowonjun <twojunspace@naver.com>
2023-12-22 17:57:53 +0900

    회원 추가, 조회, 수정

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -3,17 +3,23 @@ package jpabook.jpashop.api;
 import jakarta.validation.Valid;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
 
     /**
      * 실제 엔티티를 파라미터로 받지 않아야 한다.
@@ -26,12 +32,38 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    /**
+     * v1과 비교 시, DTO를 두게 되면 엔티티를 수정하더라도 api 스펙이 변하지 않는다.
+     * 엔티티를 외부에 노출하지 않고 api 스펙에 맞는 별도의 DTO를 설계하는 것이 중요 (유지보수성 이점)
+     * 엔티티와 표현 계층에 대한 로직을 분리할 수 있게 됨
+     * 엔티티를 파라미터에서 그대로 받거나, 외부로 노출시키지 않는다.
+     */
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
         Member member = new Member();
         member.setName(request.getName());
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable(name = "id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 
     @Data
@@ -46,9 +78,5 @@ public class MemberApiController {
     @Data
     static class CreateMemberRequest {
         private String name;
-
-        public CreateMemberRequest(String name) {
-            this.name = name;
-        }
     }
 }
